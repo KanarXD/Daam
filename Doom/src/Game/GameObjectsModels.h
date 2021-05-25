da875d6841d36887479682d78c14950446def7b8
Piotr Parzysz <piciolo@outlook.com>
2021-05-25 20:54:20 +0200

    fix

--- Doom/src/Game/GameObjectsModels.h
+++ Doom/src/Game/GameObjectsModels.h
@@ -15,7 +15,7 @@ const static std::unordered_map<std::string, ModelSpec> GOModels{
 	{ "default", {
 		{ glm::vec3(0, 1, 0), glm::vec3(1, 2, 1) },
 		{  },
-		"res/models/_default/_default.obj" , "shaderCT" } },
+		"res/models/_default/_default.obj" , "shaderCTL" } },
 	{ "player",	{
 		{ glm::vec3(0, 2, 0), glm::vec3(1, 2, 1) },
 		{ "box", "healthkit", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
@@ -23,27 +23,27 @@ const static std::unordered_map<std::string, ModelSpec> GOModels{
 	{ "enemy", {
 		{ glm::vec3(0, 0, 0), glm::vec3(1.5f) },
 		{ "box", "bullet", "player", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
-		"res/models/Enemies/enemy.obj", "shaderCT" } },
+		"res/models/Enemies/enemy.obj", "shaderCTL" } },
 	{ "enemy_fast", {
 		{ glm::vec3(0, 0, 0), glm::vec3(1.5f) },
 		{ "box", "bullet", "player", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
-		"res/models/Enemies/enemy_fast.obj", "shaderCT" } },
+		"res/models/Enemies/enemy_fast.obj", "shaderCTL" } },
 	{ "enemy_tank",	{
 		{ glm::vec3(0, 0, 0), glm::vec3(2) },
 		{ "box", "bullet", "player", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
-		"res/models/Enemies/enemy_tank.obj", "shaderCT" } },
+		"res/models/Enemies/enemy_tank.obj", "shaderCTL" } },
 	{ "enemy_boss",	{
 		{ glm::vec3(0, 0, 0), glm::vec3(2.5f) },
 		{ "box", "bullet", "player", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
-		"res/models/Enemies/enemy_boss.obj", "shaderCT" } },
+		"res/models/Enemies/enemy_boss.obj", "shaderCTL" } },
 	{ "healthkit", {
 		{ glm::vec3(0), glm::vec3(0.4f, 0.2f, 0.4f) },
 		{ "player" },
-		"res/models/Powerups/healthkit.obj", "shaderCT" } },
+		"res/models/Powerups/healthkit.obj", "shaderCTL" } },
 	{ "bullet", {
 		{ glm::vec3(0), glm::vec3(0.05f), false },
 		{ "box", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
-		"res/models/Bullet/bullet.obj", "shaderCT" } },
+		"res/models/Bullet/bullet.obj", "shaderCTL" } },
 	{ "box", { 
 		{ glm::vec3(0, 5, 0), glm::vec3(5.0f), false },
 		{ "player", "bullet", "enemy" , "enemy_fast", "enemy_tank", "enemy_boss" },
